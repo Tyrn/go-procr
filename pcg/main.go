39bc573fdec467e2d1c4413eb39ea013c449aa8a
Alexey Orlov <orlov@bos-ltd.ru>
2016-01-23 12:47:13 +0300

    MakeInitials() implemented

--- pcg/main.go
+++ pcg/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -34,6 +36,109 @@ func SansExt(pth string) string {
 func HasExtOf(pth, ext string) bool {
 	xt := strings.Trim(filepath.Ext(pth), ". ")
 	return strings.ToUpper(xt) == strings.ToUpper(strings.Trim(ext, ". "))
+}
+
+// Returns a vector of integer numbers
+// embedded in a string argument
+func StrStripNumbers(s string) []int {
+	r, _ := regexp.Compile(`\d+`)
+	var p []int
+	for _, v := range r.FindAllString(s, -1) {
+		n, _ := strconv.Atoi(v)
+		p = append(p, n)
+	}
+	if p == nil {
+		return []int{}
+	}
+	return p
+}
+
+// Compares arrays of integers using 'string semantics'
+func ArrayCmp(x, y []int) int {
+	if len(x) == 0 {
+		if len(y) == 0 {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	if len(y) == 0 {
+		if len(x) == 0 {
+			return 0
+		} else {
+			return 1
+		}
+	}
+	i := 0
+	for x[i] == y[i] {
+		if i == len(x)-1 || i == len(y)-1 {
+			// Short array is a prefix of the long one; end reached. All is equal so far.
+			if len(x) == len(y) {
+				// Long array is no longer than the short one.
+				return 0
+			}
+			if len(x) < len(y) {
+				return -1
+			}
+			return 1
+		}
+		i++
+	}
+	// Difference encountered.
+	if x[i] < y[i] {
+		return -1
+	}
+	return 1
+}
+
+// If both strings contain digits, returns numerical comparison based on the numeric
+// values embedded in the strings, otherwise returns the standard string comparison.
+// The idea of the natural sort as opposed to the standard lexicographic sort is one of coping
+// with the possible absence of the leading zeros in 'numbers' of files or directories
+func StrCmpNaturally(x, y string) int {
+	a := StrStripNumbers(x)
+	b := StrStripNumbers(y)
+	if len(a) > 0 && len(b) > 0 {
+		return ArrayCmp(a, b)
+	} else {
+		return strings.Compare(x, y)
+	}
+}
+
+// Reduces a string of names to initials
+func MakeInitials(name string) string {
+	sep := "."
+	trail := "."
+	hyph := "-"
+
+	// Remove double quoted substring, if any.
+	qr, _ := regexp.Compile(`"`)
+	quotes := qr.FindAllString(name, -1)
+	qcnt := len(quotes)
+	sr, _ := regexp.Compile(`"(.*?)"`) // Replace double quoted substrings.
+	enm := ""
+	if qcnt == 0 || qcnt%2 != 0 {
+		enm = name
+	} else {
+		enm = sr.ReplaceAllString(name, " ")
+	}
+
+	pr, _ := regexp.Compile(`\s+`) // Split by any space.
+	splitBySpace := func(nm string) []string {
+		spl := pr.Split(nm, -1)
+		var ini []string
+		for _, v := range spl {
+			ini = append(ini, strings.ToUpper(v[:1]))
+		}
+		return ini
+	}
+	fr, _ := regexp.Compile(hyph) // Split by hyph.
+	spl := fr.Split(enm, -1)
+	var r []string
+	for _, v := range spl {
+		r = append(r, strings.Join(splitBySpace(v), sep))
+	}
+	return strings.Join(r, hyph) + trail
 }
 
 func main() {
--- pcg/main_test.go
+++ pcg/main_test.go
@@ -22,3 +22,31 @@ func TestHasExtOf(t *testing.T) {
 	assert.Equal(t, true, HasExtOf("/alfa/bra.vo/charlie.ogg", ".ogg"), tsbe)
 	assert.Equal(t, false, HasExtOf("/alfa/bra.vo/charlie.ogg", "mp3"), tsbe)
 }
+
+func TestStrStripNumbers(t *testing.T) {
+	assert.Equal(t, []int{11, 2, 144}, StrStripNumbers("ab11cdd2k.144"), tsbe)
+	assert.Equal(t, []int{}, StrStripNumbers("Ignacio Vazquez-Abrams"), tsbe)
+}
+
+func TestArrayCmp(t *testing.T) {
+	assert.Equal(t, 0, ArrayCmp([]int{}, []int{}), tsbe)
+	assert.Equal(t, 1, ArrayCmp([]int{1}, []int{}), tsbe)
+	assert.Equal(t, 1, ArrayCmp([]int{3}, []int{}), tsbe)
+	assert.Equal(t, -1, ArrayCmp([]int{1, 2, 3}, []int{1, 2, 3, 4, 5}), tsbe)
+	assert.Equal(t, -1, ArrayCmp([]int{1, 4}, []int{1, 4, 16}), tsbe)
+	assert.Equal(t, 1, ArrayCmp([]int{2, 8}, []int{2, 2, 3}), tsbe)
+	assert.Equal(t, -1, ArrayCmp([]int{0, 0, 2, 4}, []int{0, 0, 15}), tsbe)
+	assert.Equal(t, 1, ArrayCmp([]int{0, 13}, []int{0, 2, 2}), tsbe)
+	assert.Equal(t, 0, ArrayCmp([]int{11, 2}, []int{11, 2}), tsbe)
+}
+
+func TestCmpStrNaturally(t *testing.T) {
+	assert.Equal(t, 0, StrCmpNaturally("", ""), tsbe)
+	assert.Equal(t, -1, StrCmpNaturally("2a", "10a"), tsbe)
+	assert.Equal(t, -1, StrCmpNaturally("alfa", "bravo"), tsbe)
+}
+
+func TestMakeInitials(t *testing.T) {
+	assert.Equal(t, "J.R.R.T.", MakeInitials("John ronald reuel Tolkien"), tsbe)
+	assert.Equal(t, "A.C-G.", MakeInitials("Apsley Cherry-Garrard"), tsbe)
+}
